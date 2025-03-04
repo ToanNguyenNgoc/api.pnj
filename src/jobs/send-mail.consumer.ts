6bf53ca2421345ceefd8df4fd381d5ed108c91e5
Nguyen Toan <ngoctoan06011998>
2025-03-04 10:51:56 +0700

    add cors

--- src/jobs/send-mail.consumer.ts
+++ src/jobs/send-mail.consumer.ts
@@ -42,7 +42,7 @@ export class SendMailConsumer {
       email,
       redirect_url: `${
         process.env.APP_URL_CLIENT
-      }?code=${this.hasHelper.createVerificationCode(email)}`,
+      }/verify-mail?code=${this.hasHelper.createVerificationCode(email)}`,
     };
     return {
       data,
